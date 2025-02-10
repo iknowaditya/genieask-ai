5b7ea339db665fb56194dd5e09cdc15ae8078762
Hanu Singh <iknowaditya16@gmail.com>
2025-02-11 04:19:10 +0530

    fix auth

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -1,4 +1,4 @@
-// components/ui/Toast.tsx
+// components/ui/toast.tsx
 import * as React from "react";
 import * as ToastPrimitives from "@radix-ui/react-toast";
 
@@ -10,26 +10,29 @@ interface ToastProps {
   type?: "default" | "success" | "error";
 }
 
+export const ToastProvider = ToastPrimitives.Provider;
+export const ToastViewport = ToastPrimitives.Viewport;
+export const ToastTitle = ToastPrimitives.Title;
+export const ToastDescription = ToastPrimitives.Description;
+export const ToastClose = ToastPrimitives.Close;
+
 export function Toast({ message, type = "default" }: ToastProps) {
   return (
-    <ToastPrimitives.Provider>
-      <ToastPrimitives.Root
-        className={cn(
-          "bg-white rounded-lg shadow-lg p-4",
-          "data-[type=success]:bg-green-50",
-          "data-[type=error]:bg-red-50",
-          "fixed bottom-4 right-4 z-50"
-        )}
-        data-type={type}
-      >
-        <div className="flex">
-          <div className="flex-1">{message}</div>
-          <ToastPrimitives.Close>
-            <X className="w-4 h-4" />
-          </ToastPrimitives.Close>
-        </div>
-      </ToastPrimitives.Root>
-      <ToastPrimitives.Viewport />
-    </ToastPrimitives.Provider>
+    <ToastPrimitives.Root
+      className={cn(
+        "bg-white rounded-lg shadow-lg p-4",
+        "data-[type=success]:bg-green-50",
+        "data-[type=error]:bg-red-50",
+        "fixed bottom-4 right-4 z-50"
+      )}
+      data-type={type}
+    >
+      <div className="flex">
+        <div className="flex-1">{message}</div>
+        <ToastPrimitives.Close>
+          <X className="w-4 h-4" />
+        </ToastPrimitives.Close>
+      </div>
+    </ToastPrimitives.Root>
   );
 }
